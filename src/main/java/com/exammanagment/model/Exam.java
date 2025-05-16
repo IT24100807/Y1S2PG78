a500131fef14325da9265e62c4db0fc26d0c7b5b
Chamodi2003 <it24100039@my.sliit.lk>
2025-05-16 15:28:22 -0700

    Create Exam.java

---
+++ src/main/java/com/exammanagment/model/Exam.java
@@ -0,0 +1,81 @@
+package com.exammanagment.model;
+
+import java.time.LocalDateTime;
+
+public class Exam {
+    private Long id;
+    private String title;
+    private String duration;
+    private String subject;
+    private String syllabus;
+    private LocalDateTime examDateTime;
+    private String status;
+
+    public Exam() {}
+
+    public Exam(Long id, String title, String duration, String subject, String syllabus, LocalDateTime examDateTime, String status) {
+        this.id = id;
+        this.title = title;
+        this.duration = duration;
+        this.subject = subject;
+        this.syllabus = syllabus;
+        this.examDateTime = examDateTime;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getSyllabus() {
+        return syllabus;
+    }
+
+    public void setSyllabus(String syllabus) {
+        this.syllabus = syllabus;
+    }
+
+    public LocalDateTime getExamDateTime() {
+        return examDateTime;
+    }
+
+    public void setExamDateTime(LocalDateTime examDateTime) {
+        this.examDateTime = examDateTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
