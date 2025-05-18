5a2686d92eb1004c1b22fd769a203e6c45a37227
Chamodi2003 <it24100039@my.sliit.lk>
2025-05-17 23:34:55 -0700

    Create Exam management Application

---
+++ src/main/java/com/exammanagment/ExamManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.exammanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamManagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamManagmentApplication.class, args);
+    }
+
+}
