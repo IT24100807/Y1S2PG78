4c34ce39d1b919042c52365c4123316401572332
Chamodi2003 <it24100039@my.sliit.lk>
2025-05-18 10:24:22 -0700

    Create Application Tests

---
+++ src/test/java/com/exammanagment/ExamManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exammanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamManagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
